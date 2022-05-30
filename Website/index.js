71d76f76f8e19c24f12aa0f1f64a402a31832ef2
D <carotte3000@yahoo.fr>
2022-05-30 11:06:28 +0200

    Update website with nice index.html

---
+++ Website/index.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    $('#skipIntro').on('click', function () {
+			startIntro.classList.add("hide");
+      intro_text1.classList.add("fast_show");
+			intro_text2.classList.add("fast_show");
+			intro_text3.classList.add("fast_show");
+			intro_video.classList.add("fast_show");
+  });
+});
+
+
+$(document).ready(function () {
+    $('#startIntro').on('click', function () {
+			startIntro.classList.add("hide");
+      intro_text1.classList.add("show");
+			intro_text2.classList.add("show");
+			intro_text3.classList.add("show");
+			intro_video.classList.add("show");
+  });
+});
