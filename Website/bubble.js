e5c721ce9221934e8ec9bb1a463fecfe588d0e49
KieranVaudaux <91135493+KieranVaudaux@users.noreply.github.com>
2022-06-02 13:06:14 +0200

    Update bubble.js

--- Website/bubble.js
+++ Website/bubble.js
@@ -646,7 +646,7 @@ function getDataBy({
   const now = svg.append("text")
       .style("font", `bold ${150}px var(--sans-serif)`)
       .style("font-variant-numeric", "tabular-nums")
-      
+      .style("fontSize","30px")
       .style('fill', 'white')
       .attr("text-anchor", "end")
       .attr("x", width - 150)
