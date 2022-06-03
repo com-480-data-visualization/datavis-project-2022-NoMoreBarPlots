e04a5adec06ce9d0b2dd413887126c36a5bdfa39
KieranVaudaux <91135493+KieranVaudaux@users.noreply.github.com>
2022-06-03 15:59:19 +0200

    organization

--- Website/bubble.js
+++ Website/bubble.js
@@ -136,22 +136,7 @@ function bubblesChart(energy, transaction, selectedContinents) {
     return data;
   }
 
-    keyframes_ = function () {
-      const keyframes = [];
-      let ka, a, kb, b;
-      for ([[ka, a], [kb, b]] of d3.pairs(datevalues)) {
-        for (let i = 0; i < k; ++i) {
-          const t = i / k;
-          keyframes.push([
-            new Date(ka * (1 - t) + kb * t),
-            rank(name => (a.get(name) || 0) * (1 - t) + (b.get(name) || 0) * t)
-          ]);
-        }
-      }
-      keyframes.push([new Date(kb), rank(name => b.get(name) || 0)]);
-      return keyframes;
-    }
-    keyframes = keyframes_();
+
 
     get_rad = function (data_) {
     if (data_[0] === undefined){
